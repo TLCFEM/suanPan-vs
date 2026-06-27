ec153dd405ff8490708d239b80a5c1227dfd62fb
Theodore Chang <tlcfem@gmail.com>
2026-06-27 18:57:46 +0200

    Reformat

--- scripts/sort.py
+++ scripts/sort.py
@@ -1,26 +1,28 @@
 import json
 
+def flatten_json(a):
+    if isinstance(a, dict):
+        return "".join(a.keys()) + "".join(flatten_json(v) for v in a.values())
+
+    if isinstance(a, list):
+        return "".join(flatten_json(v) for v in a)
+
+    return str(a)
+
 
 def sort_json(a):
     if isinstance(a, dict):
         return {k: sort_json(v) for k, v in sorted(a.items())}
 
     if isinstance(a, list):
-        head = a[0]
-        if isinstance(head, dict):
-            if "include" in head:
-                return sorted(a, key=lambda x: x["include"])
-            if "matches" in head:
-                return sorted(a, key=lambda x: x["matches"])
-            if "match" in head:
-                return sorted(a, key=lambda x: x["match"])
+        return sorted((sort_json(v) for v in a), key=flatten_json)
 
     return a
 
 
 if __name__ == "__main__":
-    with open("syntaxes/.tmLanguage.json", "r") as f:
+    with open("syntaxes/syntax.json", "r") as f:
         data = json.load(f)
 
-    with open("syntaxes/.tmLanguage.json", "w") as f:
+    with open("syntaxes/syntax.json", "w") as f:
         f.write(json.dumps(sort_json(data), indent=2))
