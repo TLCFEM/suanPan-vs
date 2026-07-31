16fdcd1218ff9dab135aea27d864e59339672dff
Theodore Chang <tlcfem@gmail.com>
2026-07-31 22:51:39 +0200

    Refactor code structure for improved readability and maintainability

--- scripts/sort.py
+++ scripts/sort.py
@@ -1,5 +1,6 @@
 import json
 
+
 def flatten_json(a):
     if isinstance(a, dict):
         return "".join(a.keys()) + "".join(flatten_json(v) for v in a.values())
@@ -10,19 +11,27 @@ def flatten_json(a):
     return str(a)
 
 
-def sort_json(a):
+def sort_json(a, sort_list: bool):
     if isinstance(a, dict):
-        return {k: sort_json(v) for k, v in sorted(a.items())}
+        return {k: sort_json(v, sort_list) for k, v in sorted(a.items())}
 
     if isinstance(a, list):
-        return sorted((sort_json(v) for v in a), key=flatten_json)
+        transformed = [sort_json(v, sort_list) for v in a]
+        if sort_list:
+            return sorted(transformed, key=flatten_json)
+        return transformed
 
     return a
 
 
-if __name__ == "__main__":
-    with open("syntaxes/syntax.json", "r") as f:
+def sort_file(file: str, sort_list: bool):
+    with open(file, "r") as f:
         data = json.load(f)
 
-    with open("syntaxes/syntax.json", "w") as f:
-        f.write(json.dumps(sort_json(data), indent=2))
+    with open(file, "w") as f:
+        f.write(json.dumps(sort_json(data, sort_list), indent=2))
+
+
+if __name__ == "__main__":
+    sort_file("syntaxes/syntax.json", True)
+    sort_file("syntaxes/snippets.json", False)
